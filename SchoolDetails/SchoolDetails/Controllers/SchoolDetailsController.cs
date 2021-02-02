b7da4bb1b1927734fabb78a682c614ad8b3e2612
Mahesh Majge <mahesh.majge@mjbtech.com>
2021-02-02 17:35:53 +0530

    Main code

---
+++ SchoolDetails/SchoolDetails/Controllers/SchoolDetailsController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolDetails.Models;
+
+namespace SchoolDetails.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolDetailsController : ControllerBase
+    {
+        private readonly SchoolDbContext _context;
+
+        public SchoolDetailsController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public  IEnumerable<SchoolDetail> GetSchoolDbSet()
+        {
+            return  _context.SchoolDbSet.ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SchoolDetail>> GetSchoolDetail(int id)
+        {
+            var schoolDetail = await _context.SchoolDbSet.FindAsync(id);
+
+            if (schoolDetail == null)
+            {
+                return NotFound();
+            }
+            return schoolDetail;
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSchoolDetail(int id, SchoolDetail schoolDetail)
+        {
+            if (id != schoolDetail.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(schoolDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchoolDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SchoolDetails
+        [HttpPost]
+        public async Task<ActionResult<SchoolDetail>> PostSchoolDetail(SchoolDetail schoolDetail)
+        {
+            _context.SchoolDbSet.Add(schoolDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchoolDetail", new { id = schoolDetail.ID }, schoolDetail);
+        }
+
+        // DELETE: api/SchoolDetails/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SchoolDetail>> DeleteSchoolDetail(int id)
+        {
+            var schoolDetail = await _context.SchoolDbSet.FindAsync(id);
+            if (schoolDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.SchoolDbSet.Remove(schoolDetail);
+            await _context.SaveChangesAsync();
+
+            return schoolDetail;
+        }
+
+        private bool SchoolDetailExists(int id)
+        {
+            return _context.SchoolDbSet.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ SchoolDetails/SchoolDetails/Models/SchoolDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolDetails.Models
+{
+    public class SchoolDetail
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactEmail { get; set; }
+        public string Website { get; set; }
+        public string LogoUrl { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
