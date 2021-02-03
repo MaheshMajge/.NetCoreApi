88d277ee8cc43ca7b1a0ef3eb380acb53df9c277
Mahesh Majge <mahesh.majge@mjbtech.com>
2021-02-03 14:58:22 +0530

    Update code for Repository

---
+++ SchoolDetails/NetCoreApi.Models/SchoolDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreApi.Models
+{
+    public class SchoolDetail
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactEmail { get; set; }
+        public string Website { get; set; }
+        public string LogoUrl { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
---
+++ SchoolDetails/NetCoreApi.Repository/ICommonRepository.cs
@@ -0,0 +1,15 @@
+﻿using NetCoreApi.Models;
+using System.Collections.Generic;
+
+namespace NetCoreApi.Repository
+{
+    public interface ICommonRepository
+    {
+        IEnumerable<SchoolDetail> GetSchoolDbSet();
+        SchoolDetail GetSchoolDetail(int id);
+        SchoolDetail PutSchoolDetail(int id, SchoolDetail schoolDetail);
+        SchoolDetail PostSchoolDetail(SchoolDetail schoolDetail);
+        bool DeleteSchoolDetail(int id);
+        bool SchoolDetailExists(int id);
+    }
+}
