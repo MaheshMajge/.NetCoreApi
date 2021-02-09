de9f4d3b666f38abbdf71b1847b1159745c5146f
Mahesh Majge <P50034987@capita.co.uk>
2021-02-09 10:12:49 +0530

    Added swagger gen

--- SchoolDetails/SchoolDetails/Startup.cs
+++ SchoolDetails/SchoolDetails/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SchoolDetails.Data;
 using SchoolDetails.Models;
 using SchoolDetails.Repository;
@@ -23,6 +24,10 @@ namespace SchoolDetails
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => 
+            { 
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NetCoreApi", Version = "v1" });
+            });
             services.AddScoped<ICommonRepository, CommonRepository>();
             services.AddDbContext<SchoolDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DbConnectionString")),ServiceLifetime.Transient);
         }
@@ -33,6 +38,8 @@ namespace SchoolDetails
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1.swagger.json", "NetCoreApi v1"));
             }
 
             app.UseHttpsRedirection();
